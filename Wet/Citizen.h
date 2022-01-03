9fef07caccff89d63309ad411ebcfed8e5fa1ec1
orgo17 <orr.gonen@gmail.com>
2022-01-03 15:46:11 +0200

    add descriptions to citizen,skill, manager h files

--- Wet/Citizen.h
+++ Wet/Citizen.h
@@ -8,6 +8,9 @@ using std::string;
 
 namespace mtm
 {
+    /**
+     * Citizen class declaration
+     */
     class Citizen{
         private:
             const int id;
@@ -16,16 +19,43 @@ namespace mtm
             const int birth_year;
         
         public:
-            //c'tors and d'tor
+            //c'tor
             Citizen(const int id, const string first_name, const string last_name, const int birth_year);
+            //d'tor
             virtual ~Citizen() = default;
+            //copy c'tor
             Citizen(const Citizen& citizen) = default;
 
             //getters
+            /**
+             * @brief Get the Id object
+             * 
+             * @return int 
+             */
             int getId() const;
+            /**
+             * @brief Get the First Name object
+             * 
+             * @return string 
+             */
             string getFirstName() const;
+            /**
+             * @brief Get the Last Name object
+             * 
+             * @return string 
+             */
             string getLastName() const;
+            /**
+             * @brief Get the Birth Year object
+             * 
+             * @return int 
+             */
             int getBirthYear() const;
+            /**
+             * @brief Get the Salary object
+             * 
+             * @return int 
+             */
             virtual int getSalary() const = 0;
 
             //virtual functions
@@ -35,11 +65,47 @@ namespace mtm
     };
 
     //overloaded operators
+    /**
+     * @brief compare citizens id by =
+     * 
+     * @return true if equal
+     * @return false if not equal
+     */
     bool operator==(const Citizen& citizen_1, const Citizen& citizen_2);
+    /**
+     * @brief compare citizens id by >
+     * 
+     * @return true citizen_1 id is bigger than citizen_2 id
+     * @return false otherwise
+     */
     bool operator>(const Citizen& citizen_1, const Citizen& citizen_2);
+    /**
+     * @brief compare citizens id by <
+     * 
+     * @return true citizen_1 id is less than citizen_2 id
+     * @return false otherwise
+     */
     bool operator<(const Citizen& citizen_1, const Citizen& citizen_2);
+    /**
+     * @brief compare citizens id by <=
+     * 
+     * @return true citizen_1 id is smaller than citizen_2 id or equal
+     * @return false citizen_1 id is bigger than citizen_2 id
+     */
     bool operator<=(const Citizen& citizen_1, const Citizen& citizen_2);
+    /**
+     * @brief compare citizens id by >=
+     * 
+     * @return true citizen_1 id is bigger than citizen_2 id or equal
+     * @return false citizen_1 id is smaller than citizen_2 id
+     */
     bool operator>=(const Citizen& citizen_1, const Citizen& citizen_2);
+    /**
+     * @brief compare citizens id by !=
+     * 
+     * @return true if not equal
+     * @return false if equal
+     */
     bool operator!=(const Citizen& citizen_1, const Citizen& citizen_2);   
 }
 
